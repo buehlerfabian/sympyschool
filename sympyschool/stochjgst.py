255eec3dd2212adb79a66c285518078aff435706
Fabian Bühler <buehler.fabian@gmx.de>
2024-10-26 10:14:47 +0200

    Vierfeldertafel implementiert

--- sympyschool/stochjgst.py
+++ sympyschool/stochjgst.py
@@ -1,4 +1,6 @@
 from sympy.stats import Binomial, density, P
+import sympy as sp
+from IPython.display import display, Math
 
 
 def binompdf(n, p, k):
@@ -29,3 +31,60 @@ def binomcdf(n, p, k):
     """
     X = Binomial("X", n=n, p=p)
     return P(X <= k)
+
+
+def vierfeldertafel(ab=None, abq=None, a=None,
+                    aqb=None, aqbq=None, aq=None,
+                    b=None, bq=None,
+                    normalized=True, print_latex=True):
+    """
+    Constructs a contingency table (Vierfeldertafel) and optionally prints
+    it in LaTeX format.
+    Parameters:
+    ab (float, optional): Value for the cell representing both A and B.
+    abq (float, optional): Value for the cell representing A and not B.
+    a (float, optional): Value for the total of A.
+    aqb (float, optional): Value for the cell representing not A and B.
+    aqbq (float, optional): Value for the cell representing neither A nor B.
+    aq (float, optional): Value for the total of not A.
+    b (float, optional): Value for the total of B.
+    bq (float, optional): Value for the total of not B.
+    print_latex (bool, optional): If True, prints the LaTeX representation of
+        the table. Defaults to True.
+    Returns:
+    dict: A dictionary of solved values if print_latex is False.
+    """
+    sa, sb, saq, sbq, sab, sabq, saqb, saqbq = (
+        sp.symbols('a b aq bq ab abq aqb aqbq'))
+    eqs = [sab+sabq-sa, saqb+saqbq-saq, sab+saqb -
+           sb, sabq+saqbq-sbq]
+    if normalized:
+        eqs.append(sa+saq-1)
+        eqs.append(sb+sbq-1)
+
+    params = {'a': a, 'b': b, 'aq': aq, 'bq': bq, 'ab': ab,
+              'abq': abq, 'aqb': aqb, 'aqbq': aqbq}
+    for symbol, value in params.items():
+        if value is not None:
+            eqs.append(sp.symbols(symbol) - value)
+    s = sp.solve(eqs, [sab, sabq, sa, saqb, saqbq, saq, sb, sbq])
+
+    # Extract the values from the dictionary `s`
+    values = [
+        [s[sab], s[sabq], s[sa]],
+        [s[saqb], s[saqbq], s[saq]],
+        [s[sb], s[sbq], s[sa]+s[saq]]
+    ]
+
+    # Create the LaTeX table
+    table = r"\begin{array}{|c|c|c|}\hline"
+    table += rf"{values[0][0]} & {values[0][1]} & {values[0][2]} \\\\ \hline"
+    table += rf"{values[1][0]} & {values[1][1]} & {values[1][2]} \\\\ \hline"
+    table += rf"{values[2][0]} & {values[2][1]} & {values[2][2]} \\\\ \hline"
+    table += r"\end{array}"
+
+    # Print the LaTeX table
+    if print_latex:
+        display(Math(table))
+    else:
+        return s
--- tests/test_stochjgst.py
+++ tests/test_stochjgst.py
@@ -1,5 +1,6 @@
 import sympyschool.stochjgst as st
 import pytest
+import sympy as sp
 
 
 def test_binompdf():
@@ -8,3 +9,27 @@ def test_binompdf():
 
 def test_binomcdf():
     assert st.binomcdf(n=50, p=0.25, k=10) == pytest.approx(0.262202310189509)
+
+
+def test_vierfeldertafel():
+    result = st.vierfeldertafel(abq=0.14, aqb=0.56, bq=0.36,
+                                normalized=True, print_latex=False)
+    assert result[sp.symbols('ab')] == pytest.approx(0.08)
+    assert result[sp.symbols('abq')] == pytest.approx(0.14)
+    assert result[sp.symbols('aqb')] == pytest.approx(0.56)
+    assert result[sp.symbols('aqbq')] == pytest.approx(0.22)
+    assert result[sp.symbols('a')] == pytest.approx(0.22)
+    assert result[sp.symbols('aq')] == pytest.approx(0.78)
+    assert result[sp.symbols('b')] == pytest.approx(0.64)
+    assert result[sp.symbols('bq')] == pytest.approx(0.36)
+
+    result = st.vierfeldertafel(ab=504, aqbq=42, a=630, b=672,
+                                normalized=False, print_latex=False)
+    assert result[sp.symbols('ab')] == 504
+    assert result[sp.symbols('a')] == 630
+    assert result[sp.symbols('b')] == 672
+    assert result[sp.symbols('abq')] == 126
+    assert result[sp.symbols('aqb')] == 168
+    assert result[sp.symbols('aqbq')] == 42
+    assert result[sp.symbols('aq')] == 210
+    assert result[sp.symbols('bq')] == 168
